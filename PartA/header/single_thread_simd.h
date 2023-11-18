5e81c32b1816c59cb47b45a2f68763d674bb5aa0
Snigdha Shekhar <snigdhashekhar@Snigdhas-MacBook-Air.local>
2023-11-18 21:07:50 +0530

    SIMD trial

--- PartA/header/single_thread_simd.h
+++ PartA/header/single_thread_simd.h
@@ -1,29 +1,113 @@
+// #include <cstdint>
+// #include <smmintrin.h>
+// #include <xmmintrin.h>
+// #include <emmintrin.h>
+// #include <immintrin.h>
+
+// // Optimize this function
+// void singleThread(int input_row, int input_col, int *input, int kernel_row,
+//                   int kernel_col, int *kernel, int output_row, int output_col,
+//                   long long unsigned int *output) {
+
+//   for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++) {  
+//       for (int kernel_j = 0; kernel_j < kernel_col; kernel_j+=4) { 
+//         for (int output_i = 0; output_i < output_row; output_i++) {
+//             for (int output_j = 0; output_j < output_col; output_j+=2) {
+//           int input_i = (output_i + 2 * kernel_i) % input_row;
+//           int input_j = (output_j + 2 * kernel_j) % input_col;
+//           __m256i vinput, vkernel;
+
+//           bool lastColCheck = (input_j + 7) < input_col;
+          
+//           if(lastColCheck)
+//             vinput = _mm256_loadu_si256(
+//                 (const __m256i_u *)&input[input_i * input_col + input_j]);
+//           else {
+//             vinput = _mm256_set_epi32(
+//                 input[input_i * input_col +
+//                       ((output_j + 1) + 2 * (kernel_j + 3)) % input_col],
+//                 input[input_i * input_col +
+//                       (output_j + 2 * (kernel_j + 3)) % input_col],
+//                 input[input_i * input_col +
+//                       ((output_j + 1) + 2 * (kernel_j + 2)) % input_col],
+//                 input[input_i * input_col +
+//                       (output_j + 2 * (kernel_j + 2)) % input_col],
+//                 input[input_i * input_col +
+//                       ((output_j + 1) + 2 * (kernel_j + 1)) % input_col],
+//                 input[input_i * input_col +
+//                       (output_j + 2 * (kernel_j + 1)) % input_col],
+//                 input[input_i * input_col +
+//                       ((output_j + 1) + 2 * kernel_j) % input_col],
+//                 input[input_i * input_col +
+//                       (output_j + 2 * kernel_j) % input_col]);
+
+//           } 
+//             vkernel = _mm256_set_epi32(
+//                 (kernel_j + 3 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 3]
+//                        : 0,
+//                 (kernel_j + 3 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 3]
+//                        : 0,
+//                 (kernel_j + 2 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 2]
+//                        : 0,
+//                 (kernel_j + 2 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 2]
+//                        : 0,
+//                 (kernel_j + 1 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 1]
+//                        : 0,
+//                 (kernel_j + 1 < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j + 1]
+//                        : 0,
+//                 (kernel_j < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j]
+//                        : 0,
+//                 (kernel_j < kernel_col)
+//                        ? kernel[kernel_i * kernel_col + kernel_j]
+//                        : 0);
+
+           
+
+//           __m256i output_vec = _mm256_mullo_epi32(vinput, vkernel);
+//           output_vec =
+//               _mm256_shuffle_epi32(output_vec, _MM_SHUFFLE(3, 1, 2, 0));
+//           __m128i sum128 =
+//               _mm_add_epi32(_mm256_castsi256_si128(output_vec),
+//                             _mm256_extracti128_si256(output_vec, 1));
+//           output[output_i * output_col + output_j] += _mm_extract_epi32(sum128, 0);
+//           output[output_i * output_col + output_j + 1] += _mm_extract_epi32(sum128, 2);
+//           output[output_i * output_col + output_j] += _mm_extract_epi32(sum128, 1);
+//           output[output_i * output_col + output_j + 1] += _mm_extract_epi32(sum128, 3);
+//         }
+//       }
+//     }
+//   }
+// }
 #include <cstdint>
-#include <smmintrin.h>
-#include <xmmintrin.h>
 #include <emmintrin.h>
 #include <immintrin.h>
+#include <smmintrin.h>
+#include <xmmintrin.h>
 
 // Optimize this function
 void singleThread(int input_row, int input_col, int *input, int kernel_row,
                   int kernel_col, int *kernel, int output_row, int output_col,
                   long long unsigned int *output) {
 
-  for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++) {  
-      for (int kernel_j = 0; kernel_j < kernel_col; kernel_j+=4) { 
-        for (int output_i = 0; output_i < output_row; output_i++) {
-            for (int output_j = 0; output_j < output_col; output_j+=2) {
-          int input_i = (output_i + 2 * kernel_i) % input_row;
-          int input_j = (output_j + 2 * kernel_j) % input_col;
-          __m256i vinput, vkernel;
+  for (int i = 0; i < output_row * output_col; ++i)
+    output[i] = 0;
 
-          bool lastColCheck = (input_j + 7) < input_col;
-          
-          if(lastColCheck)
-            vinput = _mm256_loadu_si256(
-                (const __m256i_u *)&input[input_i * input_col + input_j]);
-          else {
-            vinput = _mm256_set_epi32(
+  for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++) {   // C
+    for (int output_i = 0; output_i < output_row; output_i++) { // A
+      int input_i = (output_i + 2 * kernel_i) % input_row;
+      for (int kernel_j = 0; kernel_j < kernel_col; kernel_j += 4) {   // D
+        for (int output_j = 0; output_j < output_col; output_j += 2) { // B
+          int input_j = (output_j + 2 * kernel_j) % input_col;
+          __m256i input_vec;
+          if (input_j + 7 >= input_col) {
+            input_vec = _mm256_set_epi32(
                 input[input_i * input_col +
                       ((output_j + 1) + 2 * (kernel_j + 3)) % input_col],
                 input[input_i * input_col +
@@ -41,45 +125,35 @@ void singleThread(int input_row, int input_col, int *input, int kernel_row,
                 input[input_i * input_col +
                       (output_j + 2 * kernel_j) % input_col]);
 
-          } 
-            vkernel = _mm256_set_epi32(
-                (kernel_j + 3 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 3]
-                       : 0,
-                (kernel_j + 3 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 3]
-                       : 0,
-                (kernel_j + 2 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 2]
-                       : 0,
-                (kernel_j + 2 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 2]
-                       : 0,
-                (kernel_j + 1 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 1]
-                       : 0,
-                (kernel_j + 1 < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j + 1]
-                       : 0,
-                (kernel_j < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j]
-                       : 0,
-                (kernel_j < kernel_col)
-                       ? kernel[kernel_i * kernel_col + kernel_j]
-                       : 0);
+          } else {
+            input_vec = _mm256_loadu_si256(
+                (const __m256i_u *)&input[input_i * input_col + input_j]);
+          }
 
-           
+          int k3 = (kernel_j + 3 < kernel_col)
+                       ? kernel[kernel_i * kernel_col + kernel_j + 3]
+                       : 0;
+          int k2 = (kernel_j + 2 < kernel_col)
+                       ? kernel[kernel_i * kernel_col + kernel_j + 2]
+                       : 0;
+          int k1 = (kernel_j + 1 < kernel_col)
+                       ? kernel[kernel_i * kernel_col + kernel_j + 1]
+                       : 0;
+          int k0 = (kernel_j < kernel_col)
+                       ? kernel[kernel_i * kernel_col + kernel_j]
+                       : 0;
 
-          __m256i output_vec = _mm256_mullo_epi32(vinput, vkernel);
+          __m256i kernel_vec = _mm256_set_epi32(k3, k3, k2, k2, k1, k1, k0, k0);
+          __m256i output_vec = _mm256_mullo_epi32(input_vec, kernel_vec);
           output_vec =
               _mm256_shuffle_epi32(output_vec, _MM_SHUFFLE(3, 1, 2, 0));
           __m128i sum128 =
               _mm_add_epi32(_mm256_castsi256_si128(output_vec),
                             _mm256_extracti128_si256(output_vec, 1));
-          output[output_i * output_col + output_j] += _mm_extract_epi32(sum128, 0);
-          output[output_i * output_col + output_j + 1] += _mm_extract_epi32(sum128, 2);
-          output[output_i * output_col + output_j] += _mm_extract_epi32(sum128, 1);
-          output[output_i * output_col + output_j + 1] += _mm_extract_epi32(sum128, 3);
+          auto o1 = _mm_extract_epi32(sum128, 0) + _mm_extract_epi32(sum128, 1);
+          auto o2 = _mm_extract_epi32(sum128, 2) + _mm_extract_epi32(sum128, 3);
+          output[output_i * output_col + output_j] += o1;
+          output[output_i * output_col + output_j + 1] += o2;
         }
       }
     }
