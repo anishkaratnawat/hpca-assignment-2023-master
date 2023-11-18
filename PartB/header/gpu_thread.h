60698b654cacfc64a582fefb2551a53c569cdcbc
Snigdha Shekhar <snigdhashekhar@Snigdhas-MacBook-Air.local>
2023-11-19 01:34:56 +0530

    changing block size

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -29,7 +29,7 @@ void gpuThread(int input_row, int input_col, int *input,
                int kernel_row, int kernel_col, int *kernel,
                int output_row, int output_col, long long unsigned int *output) {
     // Define block and grid dimensions
-    dim3 blockSize(32,32); // Adjust block size as needed
+    dim3 blockSize(64,64); // Adjust block size as needed
     dim3 gridSize((output_col + blockSize.x - 1) / blockSize.x,
                   (output_row + blockSize.y - 1) / blockSize.y);
 
