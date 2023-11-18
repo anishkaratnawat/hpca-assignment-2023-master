95b9cb1d9672095eba300099068361167cba2b52
Snigdha Shekhar <snigdhashekhar@Snigdhas-MacBook-Air.local>
2023-11-19 01:31:01 +0530

    changing block size

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -30,8 +30,8 @@ void gpuThread(int input_row, int input_col, int *input,
                int output_row, int output_col, long long unsigned int *output) {
     // Define block and grid dimensions
     dim3 blockSize; // Adjust block size as needed
-    dim3 gridSize;
-    cudaOccupancyMaxPotentialBlockSize(&gridSize, &blockSize, gpuThreadKernel, 0, 0); 
+    dim3 minGridSize;
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, gpuThreadKernel, 0, 0); 
     dim3 gridSize((output_col + blockSize.x - 1) / blockSize.x,
                   (output_row + blockSize.y - 1) / blockSize.y);
 
