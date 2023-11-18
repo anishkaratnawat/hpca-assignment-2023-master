d0aab3b5ebb69a13b2c61de7aa1604fac6ba36c9
AnishkaRatnawat <anishkaratnawat@gmail.com>
2023-11-18 23:54:06 +0530

    modified:   PartB/header/gpu_thread.h

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -1,4 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <cuda_runtime.h>
+
 // Create other necessary functions here
+__global__ void dilatedConvolutionKernel(int input_row, int input_col, const int* input,
+                                         int kernel_row, int kernel_col, const int* kernel,
+                                         int output_row, int output_col, unsigned long long int* output) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (i < output_row && j < output_col) {
+        unsigned long long int sum = 0;
+
+        for (int ki = 0; ki < kernel_row; ++ki) {
+            for (int kj = 0; kj < kernel_col; ++kj) {
+                int inputIndex = (i + ki) * input_col + (j + kj);
+                int kernelIndex = ki * kernel_col + kj;
+                sum += static_cast<unsigned long long int>(input[inputIndex]) * static_cast<unsigned long long int>(kernel[kernelIndex]);
+            }
+        }
+
+        int outputIndex = i * output_col + j;
+        output[outputIndex] = sum;
+    }
+}
 
 // Fill in this function
 void gpuThread( int input_row, 
@@ -11,56 +36,31 @@ void gpuThread( int input_row,
                 int output_col, 
                 long long unsigned int *output ) 
 {
-
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (row < output_row && col < output_row) {
-        int sum = 0;
-
-        // Loop over the kernel and input matrix to perform convolution
-        for (int ki = 0; ki < kernel_col; ++ki) {
-            for (int kj = 0; kj < kernel_col; ++kj) {
-                int inputIndex = (row + ki) * input_col + (col + kj);
-                int kernelIndex = ki * kernel_col + kj;
-                sum += input[inputIndex] * kernel[kernelIndex];
-            }
-        }
-
-        // Assign the result to the output matrix
-        int outputIndex = row * output_col + col;
-        output[outputIndex] = sum;
-    }
-}
-
-// Host function to perform dilated convolution on GPU
-void dilatedConvolutionGPU(const int* h_input, const int* h_kernel, int* h_output,
-                            int inputSize, int kernelSize, int outputSize) {
-    int* d_input, * d_kernel, * d_output;
-
-    // Allocate memory on GPU
-    cudaMalloc((void**)&d_input, inputSize * inputSize * sizeof(int));
-    cudaMalloc((void**)&d_kernel, kernelSize * kernelSize * sizeof(int));
-    cudaMalloc((void**)&d_output, outputSize * outputSize * sizeof(int));
+     int* d_input, *d_kernel;
+    unsigned long long int* d_output;
+    cudaMalloc((void**)&d_input, input_row * input_col * sizeof(int));
+    cudaMalloc((void**)&d_kernel, kernel_row * kernel_col * sizeof(int));
+    cudaMalloc((void**)&d_output, output_row * output_col * sizeof(unsigned long long int));
 
     // Copy input and kernel matrices from host to device
-    cudaMemcpy(d_input, h_input, inputSize * inputSize * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_kernel, h_kernel, kernelSize * kernelSize * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_input, input, input_row * input_col * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_kernel, kernel, kernel_row * kernel_col * sizeof(int), cudaMemcpyHostToDevice);
 
-    // Define thread block and grid dimensions
-    dim3 blockDim(16, 16);
-    dim3 gridDim((outputSize + blockDim.x - 1) / blockDim.x, (outputSize + blockDim.y - 1) / blockDim.y);
+    // Define block and grid dimensions
+    dim3 blockSize(16, 16);
+    dim3 gridSize((output_row + blockSize.x - 1) / blockSize.x, (output_col + blockSize.y - 1) / blockSize.y);
 
-    // Launch the kernel
-    gpuThread<<<gridDim, blockDim>>>(d_input, d_kernel, d_output, inputSize, kernelSize, outputSize);
+    // Launch kernel
+    dilatedConvolutionKernel<<<gridSize, blockSize>>>(input_row, input_col, d_input,
+                                                       kernel_row, kernel_col, d_kernel,
+                                                       output_row, output_col, d_output);
 
-    // Copy the result back from device to host
-    cudaMemcpy(h_output, d_output, outputSize * outputSize * sizeof(int), cudaMemcpyDeviceToHost);
+    // Copy the result back to the host
+    cudaMemcpy(output, d_output, output_row * output_col * sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
 
-    // Free allocated memory on GPU
+    // Free device memory
     cudaFree(d_input);
     cudaFree(d_kernel);
     cudaFree(d_output);
-}
-
+    
 }
