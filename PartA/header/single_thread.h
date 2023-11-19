a4092017e09d3e3167d9fef22dea6f5f7e19bedd
Snigdha Shekhar <snigdhashekhar@Snigdhas-MacBook-Air.local>
2023-11-19 14:58:47 +0530

    optimised single thread code

--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -39,41 +39,39 @@ void singleThread( int input_row,
                 int output_col, 
                 long long unsigned int *output ) 
 {
+    int dilation = 2;
     for (int output_i = 0; output_i < output_row; ++output_i) {
         int output_offset = output_i * output_col;
 
         for (int output_j = 0; output_j < output_col; ++output_j) {
-            long long unsigned int res = 0;
+            long long unsigned int partial_sum = 0;
 
             // Adjust input_i to handle boundary conditions
-            int input_i = (output_i - 2 + input_row) % input_row;
+            int input_i = output_i;
 
             for (int kernel_i = 0; kernel_i < kernel_row; ++kernel_i) {
                 int kernel_offset = kernel_i * kernel_col;
 
                 // Adjust input_i to handle boundary conditions
-                if (input_i + 2 >= input_row) {
-                    input_i = (input_i + 2) % input_row;
-                } else {
-                    input_i = (input_i + 2);
-                }
 
                 int input_offset = input_i * input_col;
-                int input_j = (output_j - 2 + input_col) % input_col;
+                int input_j = output_j;
 
                 for (int kernel_j = 0; kernel_j < kernel_col; ++kernel_j) {
-                    // Adjust input_j to handle boundary conditions
-                    if (input_j + 2 >= input_col) {
-                        input_j = (input_j + 2) % input_col;
-                    } else {
-                        input_j = (input_j + 2);
-                    }
 
-                    res += input[input_offset + input_j] * kernel[kernel_offset + kernel_j];
+                    partial_sum += input[input_offset + input_j] * kernel[kernel_offset + kernel_j];
+                    // Adjust input_j to handle boundary conditionsmake
+                    input_j += dilation;
+                    if(input_j >= input_col)
+                        input_j %= input_col;
                 }
+
+                input_i += dilation;
+                if(input_i >= input_row)
+                        input_i %= input_row;
             }
 
-            output[output_offset + output_j] = static_cast<int>(res);
+            output[output_offset + output_j] = partial_sum;
         }
     }
 
